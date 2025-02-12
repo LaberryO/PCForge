2d859f8de2d43bc2c7a88defd5d893b2fc08c8f4
GCA-PC-8 <laberry@green.com.aca>
2025-02-12 19:43:57 +0900

    init PCForge

---
+++ src/main/java/org/kamjeon/pcforge/PcForgeApplication.java
@@ -0,0 +1,13 @@
+package org.kamjeon.pcforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kamjeon/pcforge/PcForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kamjeon.pcforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
