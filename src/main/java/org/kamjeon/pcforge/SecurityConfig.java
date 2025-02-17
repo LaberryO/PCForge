4153b400593152299108b20dccc82d5a2ca03979
GCA-PC-8 <laberry@green.com.aca>
2025-02-17 18:50:06 +0900

    Security 수정, Gradle 의존성 수정
    h2-console에 관한 문제 수정, merge에서 발생한 의존성 문제 수정

--- src/main/java/org/kamjeon/pcforge/SecurityConfig.java
+++ src/main/java/org/kamjeon/pcforge/SecurityConfig.java
@@ -6,17 +6,23 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-	
+
 	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception  {
-		http.authorizeHttpRequests((authorize) -> authorize.requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(authorize -> authorize.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+				// h2-console은 보안 무시
+				.csrf(csrf -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+				// h2-console의 컨텐츠는 항상 표시
+				.headers(headers -> headers.addHeaderWriter(
+						new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
 		return http.build();
 	}
-	
+
 }
