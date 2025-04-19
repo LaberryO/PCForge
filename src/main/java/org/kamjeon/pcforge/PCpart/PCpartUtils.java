3212b4c5b51315128400b77bf0454ac218082c14
LaberryO <laberryo0912@gmail.com>
2025-04-19 13:38:26 +0900

    refactor: PCpartUtils 인터페이스 방식에서 Array 방식으로 변경

--- src/main/java/org/kamjeon/pcforge/PCpart/PCpartUtils.java
+++ src/main/java/org/kamjeon/pcforge/PCpart/PCpartUtils.java
@@ -1,21 +1,20 @@
 package org.kamjeon.pcforge.PCpart;
 
+import java.util.Arrays;
+
 public class PCpartUtils {
 	public static void checkPCPart(String part) {
-		// part가 interface에서 정의한 상수에 포함되는지 확인
-		switch (part) {
-		case PCpartInterface.CPU:
-		case PCpartInterface.GPU:
-		case PCpartInterface.RAM:
-		case PCpartInterface.COMCASE:
-		case PCpartInterface.DISK:
-		case PCpartInterface.MBOARD:
-		case PCpartInterface.PSU:
-			// 정상적인 부품일 경우, 아무런 동작도 하지 않음
-			break;
-		default:
-			// 존재하지 않는 부품일 경우 예외 발생
+		String[] pcparts = getPCparts();
+		if (Arrays.asList(pcparts).contains(part)) {
+			return;
+		} else {
 			throw new IllegalArgumentException("존재하지 않는 부품입니다: " + part);
 		}
+	}
+	
+	public static String[] getPCparts() {
+		return new String[] {
+			"cpu", "ram", "gpu", "mboard", "psu", "comcase", "disk"
+		};
 	}
 }
